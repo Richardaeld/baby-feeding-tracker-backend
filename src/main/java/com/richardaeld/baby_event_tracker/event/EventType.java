9970cf5d476522ecb9ba2659e9f5a9b907fc54e6
Richard E <richardaeld@gmail.com>
2025-02-08 16:59:52 -0500

    Created Event table and required enum class

---
+++ src/main/java/com/richardaeld/baby_event_tracker/event/EventType.java
@@ -0,0 +1,14 @@
+package com.richardaeld.baby_event_tracker.event;
+
+public enum EventType {
+    BATH,
+    DIAPER,
+    FEEDING,
+    SLEEPING,
+    GROWTH,
+    MEDICATION,
+    NIGHT_CHECK,
+    PUMPING,
+    TEMPERATURE,
+    TUMMY_TIME
+}
