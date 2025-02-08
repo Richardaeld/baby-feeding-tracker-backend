4020c202dc441c9d649e7a2ad8421ae2565528bf
Richard E <richardaeld@gmail.com>
2025-02-07 22:05:10 -0500

    git init

---
+++ src/main/java/com/richardaeld/baby_event_tracker/BabyEventTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.richardaeld.baby_event_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyEventTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabyEventTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/richardaeld/baby_event_tracker/BabyEventTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richardaeld.baby_event_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabyEventTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
