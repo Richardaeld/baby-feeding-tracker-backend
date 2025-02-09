b30679659e1729edd4325e2015be798555f2c75f
Richard E <richardaeld@gmail.com>
2025-02-09 13:19:00 -0500

    updated to work with postgres and fixed create

--- src/main/java/com/richardaeld/baby_event_tracker/event/EventRepository.java
+++ src/main/java/com/richardaeld/baby_event_tracker/event/EventRepository.java
@@ -1,16 +1,11 @@
 package com.richardaeld.baby_event_tracker.event;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.Assert;
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -37,20 +32,21 @@ public class EventRepository {
     }
 
     public void create (Event event) {
-        var created = jdbcClient.sql("INSERT INTO EVENT (baby_id, event_type, start_on, end_on) VALUES (:baby_id, :event_type, :start_on, :end_on)")
-                .params("baby_id", event.baby_id())
-                .params("event_type", event.event_type())
-                .params("start_on", event.start_on())
-                .params("end_on", event.end_on())
+//        System.out.println("Event created: " + event.event_id() + " " + event.baby_id() + " " + event.event_type() + " " + event.start_on() + " " + event.end_on());
+        var created = jdbcClient.sql("INSERT INTO EVENT (baby_id, event_type, start_on, end_on) VALUES (:baby_id, :event_type::event_type_enum, :start_on, :end_on)")
+                .param("baby_id", event.baby_id())
+                .param("event_type", event.event_type().name())
+                .param("start_on", event.start_on())
+                .param("end_on", event.end_on())
                 .update();
-        Assert.state(created == 1, "Failed to create event " + event.event_id());
+        Assert.state(created == 1, "Failed to create event " + event.baby_id());
     }
 
     public void update(Event event, Integer event_id) {
         var updated = jdbcClient.sql("UPDATE EVENT SET baby_id = :baby_id,  = :event_id, event_type = :event_type, start_on = :start_on, end_on = :end_on WHERE event_id = :event_id")
                 .params(event)
                 .update();
-        Assert.state(updated == 1, "Failed to update event " + event.event_id());
+        Assert.state(updated == 1, "Failed to update event " + event.baby_id());
     }
 
     public void delete(Integer event_id) {
