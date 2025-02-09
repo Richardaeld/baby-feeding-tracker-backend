1b00dd9381526f1901c0ed0174c3af8606d82354
Richard E <richardaeld@gmail.com>
2025-02-08 19:08:27 -0500

    Added CRUD for repository

---
+++ src/main/java/com/richardaeld/baby_event_tracker/event/EventRepository.java
@@ -0,0 +1,47 @@
+package com.richardaeld.baby_event_tracker.event;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class EventRepository {
+    private List<Event> events = new ArrayList<>();
+
+    List<Event> findAll() {
+        return events;
+    }
+
+    Optional <Event> findById(Integer event_id) {
+        return events.stream()
+                .filter(event -> event.event_id() == event_id)
+                .findFirst();
+    }
+
+    void create (Event event) {
+        events.add(event);
+    }
+
+    void update (Event event, Integer event_id) {
+        Optional<Event> existingEvent = findById(event_id);
+        if (existingEvent.isPresent()) {
+            events.set(events.indexOf(existingEvent.get()), event);
+        }
+    }
+
+    void delete (Integer event_id) {
+        events.removeIf(event -> event.event_id().equals(event_id));
+    }
+
+    @PostConstruct
+    private void init() {
+        events.add(new Event(1, 1, EventType.BATH, LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS)));
+        events.add(new Event(2, 1, EventType.FEEDING, LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS)));
+        events.add(new Event(3, 1, EventType.SLEEPING, LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS)));
+    }
+}
