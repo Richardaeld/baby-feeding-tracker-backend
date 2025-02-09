9ac26594d3e9e0d4ba802785dcd00d8e69f4a47d
Richard E <richardaeld@gmail.com>
2025-02-08 19:16:58 -0500

    Added validation dependency and improved validation

--- src/main/java/com/richardaeld/baby_event_tracker/event/Event.java
+++ src/main/java/com/richardaeld/baby_event_tracker/event/Event.java
@@ -1,17 +1,26 @@
 package com.richardaeld.baby_event_tracker.event;
 
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDateTime;
 
 public record Event(
-        Integer event_id,
+        @NotEmpty
+        @Positive
         Integer baby_id,
+        @NotEmpty
+        @Positive
+        Integer event_id,
+        @NotEmpty
         EventType event_type,
         LocalDateTime start_on,
         LocalDateTime end_on
 ) {
         public Event {
                 if (!end_on.isAfter(start_on)) {
-                        threw new IllegalArgumentException("End time must be after start time");
+                        throw new IllegalArgumentException("End time must be after start time");
                 }
         }
 }
