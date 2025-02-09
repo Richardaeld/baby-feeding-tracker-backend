577b2ba513fc0aa083535b03b1cd2876ed37e1e9
Richard E <richardaeld@gmail.com>
2025-02-09 13:17:52 -0500

    Created interface to minify code used ... has issues working with postgres 'enum'

---
+++ src/main/java/com/richardaeld/baby_event_tracker/event/InterfaceEventRepository.java
@@ -0,0 +1,20 @@
+package com.richardaeld.baby_event_tracker.event;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface InterfaceEventRepository {
+    List<Event> findAll();
+
+    Optional<Event> findById(Integer event_id);
+
+    void create(Event event);
+
+    void update(Event event, Integer event_id);
+
+    void delete(Integer event_id);
+
+    int count();
+
+
+}
