6d72f63d9fe2376f1de1c6af058bb55c36c5aebb
Richard E <richardaeld@gmail.com>
2025-02-09 22:49:31 -0500

    Went back to original design

--- src/main/java/com/richardaeld/baby_event_tracker/events/Event.java
+++ src/main/java/com/richardaeld/baby_event_tracker/events/Event.java
@@ -2,12 +2,12 @@ package com.richardaeld.baby_event_tracker.events;
 
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
 import jakarta.validation.constraints.Positive;
 
 import java.time.LocalDateTime;
 
 public record Event(
-//        Parent
         Integer event_id,
         @NotNull
         @Positive
@@ -16,19 +16,7 @@ public record Event(
         EventType event_type,
         @NotNull
         LocalDateTime start_on,
-        LocalDateTime end_on,
-//        Children
-        Integer note_id,
-        String note,
-
-        Integer temperature,
-        Integer amount,
-        String dosage,
-        String name,
-        Integer height,
-        Integer weight,
-        String color
-
+        LocalDateTime end_on
 ) {
         public Event {
                 if (!end_on.isAfter(start_on)) {
